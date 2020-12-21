aaf7d00c6ed1bcdb736dfe92bccc585a8acdd63d
adamantal <adam.antal@banzaicloud.com>
2020-12-21 17:26:03 +0100

    Adding exit condition for control loop

--- src/main/java/org/conio/container/engine/ContainerEventType.java
+++ src/main/java/org/conio/container/engine/ContainerEventType.java
@@ -1,7 +1,12 @@
 package org.conio.container.engine;
 
 public enum ContainerEventType {
+  NONE,
   STARTED,
   FAILED,
-  SUCCEEDED
+  SUCCEEDED;
+
+  boolean isTerminated() {
+    return this.equals(SUCCEEDED) || this.equals(FAILED);
+  }
 }
