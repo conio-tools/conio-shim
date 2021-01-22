17be93f9a7f1133a6cea0b3925e93473bb5d9722
adamantal <aada99@gmail.com>
2021-01-22 10:41:45 +0100

    Covering more tests

---
+++ src/test/java/org/conio/container/engine/util/TestTranslate.java
@@ -0,0 +1,36 @@
+package org.conio.container.engine.util;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.FileNotFoundException;
+import java.util.Map;
+
+import com.google.common.collect.ImmutableMap;
+import org.apache.hadoop.yarn.api.records.Resource;
+import org.conio.container.engine.util.Translate;
+import org.conio.container.k8s.Container;
+import org.conio.container.k8s.Pod;
+import org.conio.container.k8s.ResourceRequirements;
+import org.junit.Test;
+
+public class TestTranslate {
+  @Test
+  public void testTranslateResourceRequirementsFromPod() throws FileNotFoundException {
+    Pod pod = Pod.parseFromFile("src/test/resources/sleep_pod.yaml");
+    Resource res = Translate.translateResourceRequirements(pod.getSpec().getContainers().get(0));
+    assertEquals(200L, res.getMemorySize());
+    assertEquals(1, res.getVirtualCores());
+  }
+
+  @Test
+  public void testTranslateResourceRequirements() {
+    Container container = new Container();
+    ResourceRequirements resreq = new ResourceRequirements();
+    Map<String, String> resources = ImmutableMap.of("cpu", "100m", "memory", "1Gi");
+    resreq.setLimits(resources);
+    container.setResources(resreq);
+    Resource res = Translate.translateResourceRequirements(container);
+    assertEquals(1024L, res.getMemorySize());
+    assertEquals(1, res.getVirtualCores());
+  }
+}
