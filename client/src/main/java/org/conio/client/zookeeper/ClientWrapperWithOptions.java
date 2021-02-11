e507eff6436303df6ccbdd614a9ea9f2edc269fa
adamantal <aadam99@gmail.com>
2021-02-11 12:53:10 +0100

    Wrapping ZK related args to ClientWrapperWithOptions

--- client/src/main/java/org/conio/client/zookeeper/ClientWrapperWithOptions.java
+++ client/src/main/java/org/conio/client/zookeeper/ClientWrapperWithOptions.java
@@ -5,6 +5,8 @@ import static org.conio.client.command.option.CLIOption.ZK_ROOT_NODE;
 
 import java.util.Arrays;
 import java.util.Collection;
+import org.apache.commons.cli.CommandLine;
+import org.conio.Constants;
 import org.conio.client.command.option.CLIOption;
 import org.conio.client.command.option.CLIOptionProvider;
 import org.conio.zookeeper.ClientWrapper;
@@ -13,5 +15,18 @@ public class ClientWrapperWithOptions extends ClientWrapper implements CLIOption
   @Override
   public Collection<CLIOption> collectCLIOptions() {
     return Arrays.asList(ZK_CLIENT, ZK_ROOT_NODE);
+  }
+
+  /**
+   * Precomputes the parameter from the command line arguments
+   * to initialize the Zookeeper client.
+   */
+  public void init(CommandLine cliParser) {
+    String zkConnectionString = cliParser.getOptionValue(ZK_CLIENT.option());
+    String zkRoot = cliParser.getOptionValue(ZK_ROOT_NODE.option());
+    if (zkRoot == null) {
+      zkRoot = Constants.DEFAULT_ZK_ROOT_NODE;
+    }
+    super.init(zkRoot, zkConnectionString);
   }
 }
